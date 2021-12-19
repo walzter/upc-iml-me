c3043d3142adb609eb8bd3162b6b19578ca02ccc
shivani-1 <14bce118@nirmauni.ac.in>
2021-12-19 22:45:35 +0100

    Added RFE feature selector, toggle to turn FS on and corresponding changes in distance metric calculation

--- Labs/IBL/main.py
+++ Labs/IBL/main.py
@@ -4,8 +4,11 @@ def run_kibl():
     dataset_name = 'autos'
     k = 5
     voting_protocol = 'modified_plurality'
-    K_IBL_classify, perf_time = kibl(dataset_name, k, voting_protocol)
-    print('Dataset =', dataset_name, '| k =', k, '| voting protocol =', voting_protocol)
+    feature_selection_flag = True
+    feature_selection_method = 'rfe'
+    no_of_features = 11
+    print('Dataset = ' + dataset_name + ' | k = ' + str(k) + ' | Voting protocol = ' + voting_protocol + ' | Feature selection on? = ' + str(feature_selection_flag)+'/'+str(feature_selection_method)+'/'+str(no_of_features)+' features')
+    K_IBL_classify, perf_time = kibl(dataset_name, k, voting_protocol, feature_selection_flag, feature_selection_method, no_of_features)
     print('Accuracy % for (Euclidean, Manhattan, Clark, HVDM) = ', evaluate_performance(K_IBL_classify))
     print('Time taken to classify (across all 10 folds) =', perf_time)
 
