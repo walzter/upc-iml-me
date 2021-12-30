1e7e1e2ebc439e224401a2335c6a2fc24fcfb8c1
andreynz691 <andreynz691@gmail.com>
2021-12-30 23:22:34 +0100

    Cleanup

--- Labs/IBL/src/main.py
+++ Labs/IBL/src/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from algorithms.kibl import kibl, evaluate_performance
+import numpy as np
 from src.algorithms.ibl1 import IBL1
 from src.algorithms.ibl2 import IBL2
 from src.algorithms.ibl3 import IBL3
@@ -46,5 +48,23 @@ def iblflow():
     ibl2(filename)
     ibl3(filename)
 
+def run_kibl():
+    dataset_name = 'soybean'
+    voting_protocol = 'modified_plurality'
+    feature_selection_flag = True
+    feature_selection_method = 'rfe'
+    threshold_or_no_of_features = 17
+
+    k_accuracy = [[] for i in range(1, 5)]
+    k_time_taken = np.zeros((9))
+    for index, k in enumerate([1, 3, 5, 7]):
+        print('Dataset = ' + dataset_name + ' | k = ' + str(k) + ' | Voting protocol = ' + voting_protocol + ' | Feature selection on? = ' + str(feature_selection_flag) + '/' + str(feature_selection_method) + '/' + str(threshold_or_no_of_features) + ' threshold/features')
+        K_IBL_classify, perf_time = kibl(dataset_name, k, voting_protocol, feature_selection_flag, feature_selection_method, threshold_or_no_of_features)
+        k_accuracy[index] = evaluate_performance(K_IBL_classify)
+        print('Accuracy % for (Euclidean, Manhattan, Clark, HVDM) =', k_accuracy[k-1])
+        k_time_taken[index] = perf_time
+        print('Time taken to classify (across all 10 folds) =', perf_time)
+
 if __name__ == '__main__':
     iblflow()
+    #run_kibl()
--- Labs/IBL/src/similarities/similarities.py
+++ Labs/IBL/src/similarities/similarities.py
@@ -2,30 +2,17 @@ import numpy as np
 import pandas as pd
 import numbers
 
+
 def euclidean_sim(x_pred, x_train):
-    # TODO Split data between numerical and categorical
-    # Apply xp[i] != xt[i] so 0 if equals, 1 otherwise
-    # Apply euclidean for numerical + categorical
-    # Possible options would be:
-    #   - Check each element type in the array and perform op
-    #   - Split ds in numerical and categorical, this improves perf
-    #   - Apply onehot encoding
     def only_numeric(lst):
         only_nums = [x for x in lst if isinstance(x, numbers.Number)]
         only_nums = pd.Series(only_nums)
         return only_nums
 
-
-    #x_pred_num = x_pred._get_numeric_data()
     x_pred_num = only_numeric(x_pred)
-    #x_train_num = x_train._get_numeric_data()
     x_train_num = only_numeric(x_train)
     x_pred_cat = list(set(x_pred) - set(x_pred_num))
     x_train_cat = list(set(x_train) - set(x_train_num))
-
-    # Missing how to deal with missing data
-    x_pred_miss = np.where(x_pred.isna())
-    x_train_miss = np.where(x_train.isna())
 
     num_diff = x_pred_num - x_train_num if x_pred_num.shape[0] > 0 else []
     cat_diff = [1 if cat_a != cat_b else 0 for cat_a, cat_b in zip(x_pred_cat, x_train_cat)]
